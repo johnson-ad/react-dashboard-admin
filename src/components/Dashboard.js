090da7dd6675ee59eb8c735294242a67f3c0701b
='Johnson-ad <=>
2022-07-08 14:09:32 +0000

    Ajout du style aux component dashboard et navbar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -28,6 +28,35 @@ export default function Dashboard() {
 
 
 const Section = styled.section`
-
-
-`
+  margin-left: 18vw;
+  padding: 2rem;
+  height: 100%;
+  .grid {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap: 1rem;
+    margin-top: 2rem;
+    .row__one {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      height: 50%;
+      gap: 1rem;
+    }
+    .row__two {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      gap: 1rem;
+      height: 50%;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    margin-left: 0;
+    .grid {
+      .row__one,
+      .row__two {
+        grid-template-columns: 1fr;
+      }
+    }
+  }
+`;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
                 <h4>Hi Johnson,</h4>
                 <h1>Welcome to <span>My Taxi DASHBOARD</span></h1>
             </div>
-            <div className='nav-items'>
+            <div className='search'>
                 <BiSearch />
                 <input type="text" placeholder='Search' />
             </div>
@@ -18,5 +18,54 @@ export default function Navbar() {
 }
 
 const Nav = styled.nav`
-
-`
+  display: flex;
+  justify-content: space-between;
+  color: white;
+  .title {
+    h1 {
+      span {
+        margin-left: 0.5rem;
+        color: #ffc107;
+        font-family: "Permanent Marker", cursive;
+        letter-spacing: 0.2rem;
+      }
+    }
+  }
+  .search {
+    background-color: #212121;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem 8rem 1rem 1rem;
+    border-radius: 1rem;
+    svg {
+      color: #ffc107;
+    }
+    input {
+      background-color: transparent;
+      border: none;
+      color: #ffc107;
+      font-family: "Permanent Marker", cursive;
+      letter-spacing: 0.3rem;
+      &:focus {
+        outline: none;
+      }
+      &::placeholder {
+        color: #ffc107;
+        font-family: "Permanent Marker", cursive;
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    flex-direction: column;
+    .title {
+      h1 {
+        span {
+          display: block;
+          margin: 1rem 0;
+          /* letter-spacing: 0; */
+        }
+      }
+    }
+  }
+`;
