60540b6b69a128418a0767d15dae8669aa0a8e44
='Johnson-ad <=>
2022-07-08 12:11:53 +0000

    Creation de sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+import { MdSpaceDashboard } from "react-icons/md";
+import { RiDashboard2Fill } from "react-icons/ri";
+import { FaAddressCard, FaTaxi } from "react-icons/fa";
+import { GiTwirlCenter } from "react-icons/gi";
+import { BsFillChatTextFill } from "react-icons/bs";
+import { IoSettings } from "react-icons/io5";
+import { FiLogOut } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { VscChromeClose } from "react-icons/vsc";
+
+export default function Sidebar() {
+    return (
+        <div>Sidebar</div>
+    )
+}
