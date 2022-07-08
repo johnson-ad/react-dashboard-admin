897063b450adb11eb550ecc3f059b401b804b64f
='Johnson-ad <=>
2022-07-08 13:33:21 +0000

    Creation du dashboard et navbar component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+import Navbar from './Navbar'
+import Analytics from './Analytics'
+import FAQ from './FAQ'
+import Earnings from './Earnings'
+import Transfers from './Transfers'
+import Profile from './Profile'
 
 export default function Dashboard() {
     return (
-        <div>Dashboard</div>
+        <Section>
+            <Navbar />
+            <div className='grid'>
+                <div className='row__one'>
+                    <Analytics />
+                    <FAQ />
+                </div>
+                <div className='row__two'>
+                    <Earnings />
+                    <Transfers />
+                    <Profile />
+                </div>
+            </div>
+        </Section>
     )
 }
+
+
+const Section = styled.section`
+
+
+`
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import { BiSearch } from 'react-icons/bi'
 
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <Nav>
+            <div className='title'>
+                <h4>Hi Johnson,</h4>
+                <h1>Welcome to <span>My Taxi DASHBOARD</span></h1>
+            </div>
+            <div className='nav-items'>
+                <BiSearch />
+                <input type="text" placeholder='Search' />
+            </div>
+        </Nav>
     )
 }
+
+const Nav = styled.nav`
+
+`
