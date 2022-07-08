1c4409c510719eda97b7ce6d7e93d691f219054b
='Johnson-ad <=>
2022-07-08 12:11:53 +0000

    Creation de sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+import Dashboard from './components/Dashboard'
+import Sidebar from './components/Sidebar'
+
 
 export default function App() {
   return (
-    <div>Hello</div>
+    <div>
+      <Sidebar />
+      <Dashboard />
+    </div>
   )
 }
